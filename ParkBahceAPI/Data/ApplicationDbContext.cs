5bc8e6763a27f489fdc6a92000db1585d12c6c8e
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-18 02:06:58 +0300

    DbSet Context

---
+++ ParkBahceAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkBahceAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<MilletBahcesi> MilletBahcesis { get; set; }
+    }
+  
+}
