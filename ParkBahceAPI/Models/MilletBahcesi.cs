bef615acf6143eb1c2658cea88b8dcc9622490fb
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-18 02:05:28 +0300

    Context

---
+++ ParkBahceAPI/Models/MilletBahcesi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Models
+{
+    public class MilletBahcesi
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
