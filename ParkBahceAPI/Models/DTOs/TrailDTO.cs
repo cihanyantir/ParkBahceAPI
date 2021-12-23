4585ead5595ec4911e9947e4394df5f32eaf29f2
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-23 17:57:53 +0300

    Data Transfer Object

---
+++ ParkBahceAPI/Models/DTOs/TrailDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkBahceAPI.Models.Trail;
+
+namespace ParkBahceAPI.Models.DTOs
+{
+    public class TrailDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+    
+        public DifficultType Difficulty { get; set; }
+        public int MilletBahcesiId { get; set; }
+        public MilletBahcesiDTO MilletBahcesi { get; set; }
+    }
+}
