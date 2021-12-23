f9b880beaadf9c3b0d2151f7edd92f7f68e0de30
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-23 17:57:19 +0300

    Models Added

--- ParkBahceAPI/Models/MilletBahcesi.cs
+++ ParkBahceAPI/Models/MilletBahcesi.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ParkBahceAPI.Models
 {
+#pragma warning disable CS1591
     public class MilletBahcesi
     {
         [Key]
@@ -18,4 +19,5 @@ namespace ParkBahceAPI.Models
         public byte[] Picture { get; set; }
         public DateTime Established { get; set; }
     }
+#pragma warning restore CS1591
 }
---
+++ ParkBahceAPI/Models/Trail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+        public enum DifficultType { Easy,Moderate,Difficult,Expert}
+        public DifficultType Difficulty { get; set; }
+        public int MilletBahcesiId { get; set; }
+        [ForeignKey("MilletBahcesiId")] //ef 5.0 can do it easily
+        public MilletBahcesi MilletBahcesi { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
