d0e3642b18f3c07ed3e5a3aebd54a5333b0c19a8
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-23 18:56:17 +0300

    Trail Inferface Added

---
+++ ParkBahceAPI/Abstract/ITrailRepository.cs
@@ -0,0 +1,22 @@
+﻿using ParkBahceAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Abstract
+{
+    public interface ITrailRepository
+    {
+        ICollection<Trail> GetTrails();
+        ICollection<Trail> GetTrailInNationalPark(int mbID);
+        Trail GetTrail(int TrailID);
+        bool TrailExist(string name);
+        bool TrailExist(int id);
+        bool CreateTrail(Trail Trail);
+        bool UpdateTrail(Trail Trail);
+        bool DeleteTrail(Trail Trail);
+        bool Save();
+
+    }
+}
