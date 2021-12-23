e486643e2fc633b2d1faac0873a720783b4fb0b3
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-23 21:14:26 +0300

    Create and Update Data Object Transfer Added

---
+++ ParkBahceAPI/Models/DTOs/TrailUpdateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkBahceAPI.Models.Trail;
+
+namespace ParkBahceAPI.Models.DTOs
+{
+    public class TrailUpdateDTO
+    {
+     
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+
+        public DifficultType Difficulty { get; set; }
+        public int MilletBahcesiId { get; set; }
+    }
+}
