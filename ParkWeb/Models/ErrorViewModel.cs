e5eb29eca6c20dfa4fd2358a66d5302c6b9d993e
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-25 19:35:58 +0300

    MVC Application Added

---
+++ ParkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ParkWeb/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkWeb
+{
+    public class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44300/";
+        public static string MilletBahcesiAPIPath = "https://localhost:44300/";
+        public static string APIBaseUrl = "https://localhost:44300/";
+    }
+}
