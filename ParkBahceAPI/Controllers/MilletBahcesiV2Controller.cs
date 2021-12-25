42d1a2110f2975b942b9b893321e2c3950baa801
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-25 19:08:39 +0300

    Controller Versioning

---
+++ ParkBahceAPI/Controllers/MilletBahcesiV2Controller.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkBahceAPI.Abstract;
+using ParkBahceAPI.Models;
+using ParkBahceAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/milletbahcesi")]
+    [ApiVersion("2.0")]
+    [ApiController]
+    //[ApiExplorerSettings(GroupName = "ParkBahceAPIMilletBahcesi")]
+    public class MilletBahcesiV2Controller : Controller
+    {
+        private IMilletBahcesiRepository _mbRepo;
+        private readonly IMapper _mapper;
+        public MilletBahcesiV2Controller(IMilletBahcesiRepository mbrepo, IMapper mapper)
+        {
+            _mbRepo = mbrepo;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Millet Bahcesi Listesi
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetMilletBahcesi()
+        {
+            var obj = _mbRepo.GetMilletBahcesis().FirstOrDefault();
+
+       
+            return Ok(_mapper.Map<MilletBahcesiDTO>(obj)); //bana maplanana eklenmiş veriyi getir.
+
+        }
+    }
+    
+}
