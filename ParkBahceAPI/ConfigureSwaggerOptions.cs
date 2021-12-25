abf70ad224acb27215541b86620fc18f54fd0aee
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-12-25 19:08:22 +0300

    Swagger Doc by API Versioning.

---
+++ ParkBahceAPI/ConfigureSwaggerOptions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var desc in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(
+                  desc.GroupName, new Microsoft.OpenApi.Models.OpenApiInfo()
+                  {
+                      Title = $"ParkBahce API {desc.ApiVersion}",
+                      Version = desc.ApiVersion.ToString()
+
+                  });
+
+            }
+            var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+            options.IncludeXmlComments(cmlCommentsFullPath); //addxml
+        }
+
+       
+    }
+}
