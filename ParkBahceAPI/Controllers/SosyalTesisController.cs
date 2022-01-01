4071f3b54a20aa5945a5ff498289adf1d610f0fa
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-01 23:35:58 +0300

    Sosyal Tesis Controller

---
+++ ParkBahceAPI/Controllers/SosyalTesisController.cs
@@ -0,0 +1,144 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkBahceAPI.Abstract;
+using ParkBahceAPI.Models;
+using ParkBahceAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/SosyalTesis")]
+    //[ApiExplorerSettings(GroupName = "ParkBahceAPISosyalTesis")]
+    [ApiController]
+    public class SosyalTesisController : Controller
+    {
+        private ISosyalTesisRepository _SosyalTesisrepo;
+        private readonly IMapper _mapper;
+        public SosyalTesisController(ISosyalTesisRepository mbrepo, IMapper mapper)
+        {
+            _SosyalTesisrepo = mbrepo;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// SosyalTesis Listesi
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetSosyalTesis()
+        {
+            var objList = _SosyalTesisrepo.GetSosyalTesiss();
+            var objdto = new List<SosyalTesisDTO>();
+            foreach (var obj in objList)
+            {
+                objdto.Add(_mapper.Map<SosyalTesisDTO>(obj));
+
+            }
+            return Ok(objdto);
+
+        }
+        /// <summary>
+        /// ID'ye göre SosyalTesis
+        /// </summary>
+        /// <param name="SosyalTesisID">Seçilen ID</param>
+        /// <returns></returns>
+        [HttpGet("{SosyalTesisID:int}", Name = "GetSosyalTesis")]
+        public IActionResult GetSosyalTesis(int SosyalTesisID)
+        {
+            var obj = _SosyalTesisrepo.GetSosyalTesis(SosyalTesisID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var objdto = _mapper.Map<SosyalTesisDTO>(obj); //no foreach needed just 1 column
+            return Ok(objdto);
+        }
+
+
+        [HttpGet("[action]/{milletbahcesiID:int}")]
+        public IActionResult GetSosyalTesisInMilletBahcesi(int milletbahcesiID)
+        {
+            var obj = _SosyalTesisrepo.GetSosyalTesisInMilletBahcesi(milletbahcesiID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var objdto = new List<SosyalTesisDTO>();
+                   
+            foreach (var item in obj)
+            {
+                objdto.Add( _mapper.Map<SosyalTesisDTO>(item));
+            }
+         
+            return Ok(objdto);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult CreateSosyalTesis([FromBody] SosyalTesisCreateDTO SosyalTesisDTO)
+        {
+            if (SosyalTesisDTO == null)
+            { return BadRequest(ModelState); }
+            if (_SosyalTesisrepo.SosyalTesisExist(SosyalTesisDTO.Name))
+            {
+                ModelState.AddModelError("", "Millet Bahcesi Exist!");
+                return StatusCode(404, ModelState);
+            }
+
+
+            var dtoobj = _mapper.Map<SosyalTesis>(SosyalTesisDTO); //dtoobj mapped (SosyalTesisDTO=SosyalTesis)
+            
+            
+            
+            if (!_SosyalTesisrepo.CreateSosyalTesis(dtoobj))//formdan gelen (maplanan) post created
+            {
+                ModelState.AddModelError("", $"  {dtoobj.Name} objesini kaydederken hata oluştu");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtRoute("GetSosyalTesis", new { SosyalTesisID = dtoobj.Id }, dtoobj);
+
+        }
+        [HttpPatch("{SosyalTesisID:int}", Name = "UpdateSosyalTesis")]
+
+        public IActionResult UpdateSosyalTesis(int SosyalTesisID, [FromBody] SosyalTesisUpdateDTO SosyalTesisDTO)
+        {
+
+            if (SosyalTesisDTO == null || SosyalTesisID != SosyalTesisDTO.Id)
+            {
+                return BadRequest(ModelState);
+            }
+            var dtoobj = _mapper.Map<SosyalTesis>(SosyalTesisDTO); //formdan geleni SosyalTesisne post
+            if (!_SosyalTesisrepo.UpdateSosyalTesis(dtoobj))
+            {
+                ModelState.AddModelError("", $"  {dtoobj.Name} objesini kaydederken hata oluştu");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+
+        }
+        [HttpDelete("{SosyalTesisID:int}", Name = "DeleteSosyalTesis")]
+        public IActionResult DeleteSosyalTesis(int SosyalTesisID)
+        {
+            if(!_SosyalTesisrepo.SosyalTesisExist(SosyalTesisID))
+            {
+                return NotFound();
+            }
+            var dtoobj = _SosyalTesisrepo.GetSosyalTesis(SosyalTesisID); 
+            if (!_SosyalTesisrepo.DeleteSosyalTesis(dtoobj)) //getile cagrilan veriyi sil
+            {
+                ModelState.AddModelError("", $"  {dtoobj.Name} objesini silerken hata oluştu");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+            
+
+        }
+
+    }
+    
+}
