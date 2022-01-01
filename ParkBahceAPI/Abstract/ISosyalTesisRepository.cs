274e20795b472272113d1ca2a5d05315d3bac18d
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-01 23:35:44 +0300

    SosyalTesis Interface

---
+++ ParkBahceAPI/Abstract/ISosyalTesisRepository.cs
@@ -0,0 +1,22 @@
+﻿using ParkBahceAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Abstract
+{
+    public interface ISosyalTesisRepository
+    {
+        ICollection<SosyalTesis> GetSosyalTesiss();
+        ICollection<SosyalTesis> GetSosyalTesisInMilletBahcesi(int mbID);
+        SosyalTesis GetSosyalTesis(int SosyalTesisID);
+        bool SosyalTesisExist(string name);
+        bool SosyalTesisExist(int id);
+        bool CreateSosyalTesis(SosyalTesis SosyalTesis);
+        bool UpdateSosyalTesis(SosyalTesis SosyalTesis);
+        bool DeleteSosyalTesis(SosyalTesis SosyalTesis);
+        bool Save();
+
+    }
+}
