678c60d5700839e4fd1b775f749fa551f1a8d1ad
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-02 19:40:13 +0300

    WebRepository

---
+++ ParkWeb/Repository/MilletBahcesiRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkWeb.Models;
+using ParkWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkWeb.Repository
+{
+    public class MilletBahcesiRepository:GenericRepository<MilletBahcesi>, IMilletBahcesiRepository
+    {
+        private readonly IHttpClientFactory _clientfactory;
+
+        public MilletBahcesiRepository(IHttpClientFactory clientfactory):base (clientfactory)
+        {
+            _clientfactory = clientfactory;
+        }
+    }
+}
