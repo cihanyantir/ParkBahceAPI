2d147cfda17dcfaa17fe0feb26884b23c645f34d
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-02 19:30:44 +0300

    Added migrations

---
+++ ParkBahceAPI/Migrations/20211229170307_SosyalTesisPicture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkBahceAPI.Migrations
+{
+    public partial class SosyalTesisPicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Picture",
+                table: "SosyalTesiss",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "SosyalTesiss");
+        }
+    }
+}
