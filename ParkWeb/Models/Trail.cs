bc050e911d4db8922a4f418980fd0337620bb1da
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-02 19:40:00 +0300

    WebModels

---
+++ ParkWeb/Models/Trail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkWeb.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+        public enum DifficultType { Easy, Moderate, Difficult, Expert }
+        public DifficultType Difficulty { get; set; }
+        public int MilletBahcesiId { get; set; }
+        public MilletBahcesi MilletBahcesi { get; set; }
+    }
+}
