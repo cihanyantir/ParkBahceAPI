b797c885e64b5c044b1b7bf71bbeeec1c6526d17
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-02 19:31:43 +0300

    Added DTO for SosyalTesis

---
+++ ParkBahceAPI/Models/DTOs/SosyalTesisCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkBahceAPI.Models.SosyalTesis;
+
+namespace ParkBahceAPI.Models.DTOs
+{
+    public class SosyalTesisCreateDTO
+    {
+       
+        [Required]
+        public string Name { get; set; }
+
+
+        public byte[] Picture { get; set; }
+        public DifficultType Doluluk { get; set; }
+        public int MilletBahcesiId { get; set; }
+      
+    }
+}
