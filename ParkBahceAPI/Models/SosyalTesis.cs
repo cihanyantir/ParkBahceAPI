abb34c5cfa39a17aff8b9a31e1d01935d76bcb63
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-01-02 19:32:09 +0300

    Added Model for SosyalTesis

---
+++ ParkBahceAPI/Models/SosyalTesis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Models
+{
+    public class SosyalTesis
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public enum DifficultType { Boş, Orta, Yüksek, Dolu }
+        public DifficultType Doluluk { get; set; }
+        public int MilletBahcesiId { get; set; } 
+        [ForeignKey("MilletBahcesiId")] 
+        public MilletBahcesi MilletBahcesi { get; set; }
+        public DateTime DateCreated { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
