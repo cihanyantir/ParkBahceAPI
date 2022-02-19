63f972e7c257b7d1b789081e2de00815f480ca8e
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-02-19 21:56:39 +0300

    Auth Model

---
+++ ParkBahceAPI/Models/AuthenticationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkBahceAPI.Models
+{
+    public class AuthenticationModel
+    {   [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
