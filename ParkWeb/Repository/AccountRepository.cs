1ffd59574fa4cf37fe1618048deef72268e862c9
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-02-19 22:01:38 +0300

    Repository changing,adding

---
+++ ParkWeb/Repository/AccountRepository.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using ParkWeb.Models;
+using ParkWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkWeb.Repository
+{
+    public class AccountRepository : GenericRepository<User>,IAccountRepository
+    {
+
+        private readonly IHttpClientFactory _clientfactory;
+
+        public AccountRepository(IHttpClientFactory clientfactory) : base(clientfactory)
+        {
+            _clientfactory = clientfactory;
+        }
+
+        public async Task<User> LoginAsync(string url, User objToCreate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else return new User();
+            var client = _clientfactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonstring = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<User>(jsonstring);
+            }
+            else return new User();
+        }
+
+        public async Task<bool> RegisterAsync(string url, User objToCreate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else return false;
+            var client = _clientfactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
